46ee852f1bc9c702030669c65acaae0ee7674944
Raphael Cassimiro <raphaelcmelob.b@gmail.com>
2023-04-03 15:50:20 -0300

    feat: add toolbar responsivity

--- src/shared/layouts/BaseLayout.tsx
+++ src/shared/layouts/BaseLayout.tsx
@@ -12,26 +12,34 @@ interface IBaseLayoutProps {
 export const BaseLayout: React.FC<IBaseLayoutProps> = ({ children, title, toolbar }) => {
 	const theme = useTheme();
 	const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+	const mdDown = useMediaQuery(theme.breakpoints.down('md'));
 	const { toggleDrawerOpen } = useDrawerContext();
 
 	return (
 		<Box height='100%' display="flex" flexDirection="column" gap={1}>
-			<Box padding={1} height={theme.spacing(12)} display="flex" alignItems="center" gap={1}>
+			<Box padding={1} display="flex" alignItems="center" gap={1} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}>
 				{smDown && (
 					<IconButton onClick={toggleDrawerOpen}>
 						<Icon>menu</Icon>
 					</IconButton>)}
 
-				<Typography variant='h5'>
+				<Typography
+					overflow="hidden"
+					whiteSpace="nowrap"
+					textOverflow="ellipses"
+					variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+				>
 					{title}
 				</Typography>
 			</Box>
 
-			<Box>
-				{toolbar}
-			</Box>
+			{toolbar && (
+				<Box>
+					{toolbar}
+				</Box>
+			)}
 
-			<Box>
+			<Box flex={1} overflow="auto">
 				{children}
 			</Box>
 		</Box>
