a3d61a7d1dcd8080dd679649a589bed637cdb2f2
Raphael Cassimiro <raphaelcmelob.b@gmail.com>
2023-04-12 14:25:24 -0300

    feat: create UseDebounce

--- src/pages/people/PeopleList.tsx
+++ src/pages/people/PeopleList.tsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from 'react-router-dom';
 import { useEffect, useMemo } from 'react';
+import { useDebounce } from '../../shared/hooks';
 
 import { PeopleService } from '../../shared/services/api/people/PeopleService';
 import { ListTools } from '../../shared/components';
@@ -7,20 +8,24 @@ import { BaseLayout } from '../../shared/layouts';
 
 export const PeopleList: React.FC = () => {
 	const [searchParams, setSearchParams] = useSearchParams();
+	const { debounce } = useDebounce(1000);
 
 	const find = useMemo(() => {
 		return searchParams.get('find') || '';
 	}, [searchParams]);
 
 	useEffect(() => {
-		PeopleService.getAll(1, find)
-			.then((result) => {
-				if (result instanceof Error) {
-					alert(result.message);
-				} else {
-					console.log(result);
-				}
-			});
+
+		debounce(() => {
+			PeopleService.getAll(1, find)
+				.then((result) => {
+					if (result instanceof Error) {
+						alert(result.message);
+					} else {
+						console.log(result);
+					}
+				});
+		});
 	}, [find]);
 
 	return (
