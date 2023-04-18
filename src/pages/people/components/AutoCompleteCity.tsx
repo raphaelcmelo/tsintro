029d76271e75f3b49a97c5f7216e5c6d99b7811e
raphaelcmelo <raphaelcmelob.b@gmail.com>
2023-04-17 21:12:57 -0300

    feat: autocomplete pt2

--- src/pages/people/components/AutoCompleteCity.tsx
+++ src/pages/people/components/AutoCompleteCity.tsx
@@ -1,24 +1,41 @@
-import { Autocomplete, TextField } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Autocomplete, CircularProgress, TextField } from '@mui/material';
+import { useEffect, useMemo, useState } from 'react';
 
 import { CityService } from '../../../shared/services/api/city/CityService';
 import { useDebounce } from '../../../shared/hooks';
+import { useField } from '@unform/core';
+import { getConstantValue } from 'typescript';
 
 type TAutoCompleteOption = {
 	id: number;
 	label: string;
 }
 
-export const AutoCompleteCity: React.FC = () => {
+interface IAutocompleteCityProps {
+  isExternalLoading?: boolean
+}
+
+export const AutoCompleteCity: React.FC<IAutocompleteCityProps> = ({ isExternalLoading = false}) => {
+	const { fieldName, defaultValue, registerField,error,clearError} = useField('cityId');
 	const { debounce } = useDebounce();
 	const [options, setOptions] = useState<TAutoCompleteOption[]>([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [find, setFind] = useState('');
+	const [selectedId, setSelectedId] = useState<number | undefined>(undefined);
+
+	useEffect(() => {
+		registerField({
+			name: fieldName,
+			getValue:  () => selectedId,
+			setValue: (_, newSelectedId) => setSelectedId(newSelectedId)
+		});
+	}, [registerField, fieldName, selectedId, ]);
 
 	useEffect(() => {
 		setIsLoading(true);
 
 		debounce(() => {
-			CityService.getAll(1/* , find */)
+			CityService.getAll(1, find)
 				.then((result) => {
 					setIsLoading(false);
 
@@ -32,9 +49,30 @@ export const AutoCompleteCity: React.FC = () => {
 		});
 	});
 
+	const autoCompleteSelectedOption = useMemo(() => {
+		if (!selectedId) return null;
+
+		const selectedOption = options.find(option => option.id === selectedId);
+		if(!selectedId) return null;
+		return selectedOption;
+	}, [selectedId, options]);
+
 	return(
 		<Autocomplete
-			options={[]}
+			openText='Abrir'
+			closeText='Fechar'
+			noOptionsText='Sem opções'
+			loadingText='Carregando...'
+
+			disablePortal
+
+			value={autoCompleteSelectedOption}
+			disabled={isExternalLoading}
+			loading={isLoading}
+			popupIcon={ (isExternalLoading || isLoading) ? <CircularProgress size={28}/> : undefined }
+			onInputChange={(_, newValue) => setFind(newValue)}
+			onChange={(_, newValue) => {setSelectedId(newValue?.id); setFind('');}}
+			options={options}
 			renderInput={(params) => (
 				<TextField
 					{...params}
